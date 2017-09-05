e075b18f87ed3e5e1f411b6f0c873eaca2a23f5f
Oscar Gomez <ogomez@stratio.com>
2017-09-05 14:24:09 +0200

    getNodeVersion test

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -1,4 +1,5 @@
 
+
 def call(body) {
 //    stage('Promote to Production repo') {
 //        milestone label: 'promote to production'
@@ -26,62 +27,72 @@ def call(body) {
     body.delegate = config
     body()
 
-    stages {
-        stage('build') {
-            steps {
-                script {
-                    echo yarnBuilder()
-                }
-            }
-        }
+    node {
+        // Clean workspace before doing anything
+        deleteDir()
+        try {
 
-        stage('publish untagged build') {
-            when {
-                expression { env.TAG_NAME == null || !(env.TAG_NAME ==~ /^v\d+\.\d+\.\d+$/) }
-            }
-            steps {
-                echo 'Start push image to repo'
-
-                script {
-                    // def props = readJSON file: 'package.json'
-                    // def buildversion = props.version + '.build-' + env.BUILD_NUMBER
-                    echo dockerBuilder('ogomezstratio','test-node-app','ogomezstratio','og1108al','v1.0.0')
+            stages {
+                stage('build') {
+                    steps {
+                        script {
+                            echo yarnBuilder()
+                        }
+                    }
                 }
 
-                echo 'End push image to repo'
-            }
-        }
+                stage('publish untagged build') {
+                    when {
+                        expression { env.TAG_NAME == null || !(env.TAG_NAME ==~ /^v\d+\.\d+\.\d+$/) }
+                    }
+                    steps {
+                        echo 'Start push image to repo'
 
-        stage('publish tagged release') {
-            when {
-                expression { env.TAG_NAME ==~ /^v\d+\.\d+\.\d+$/ }
-            }
-            steps {
-                echo 'Start push image to repo 2'
+                        script {
+                            // def props = readJSON file: 'package.json'
+                            // def buildversion = props.version + '.build-' + env.BUILD_NUMBER
+                            echo dockerBuilder('ogomezstratio', 'test-node-app', 'ogomezstratio', 'og1108al', 'v1.0.0')
+                        }
 
-                script {
-                    def buildversion = env.TAG_NAME
-                    echo dockerBuilder('ogomezstratio','test-node-app','ogomezstratio','og1108al',buildversion)
-                    // sh 'docker login sania.azurecr.io -u $ACRCRED_USR -p $ACRCRED_PSW'
-                    // sh 'docker tag sanitas-dental-bot-clinics sania.azurecr.io/sanitas-dental-bot-clinics:' + buildversion
-                    // sh 'docker push sania.azurecr.io/sanitas-dental-bot-clinics:' + buildversion
+                        echo 'End push image to repo'
+                    }
                 }
 
-                echo 'End push image to repo 2'
+                stage('publish tagged release') {
+                    when {
+                        expression { env.TAG_NAME ==~ /^v\d+\.\d+\.\d+$/ }
+                    }
+                    steps {
+                        echo 'Start push image to repo 2'
+
+                        script {
+                            def buildversion = env.TAG_NAME
+                            echo dockerBuilder('ogomezstratio', 'test-node-app', 'ogomezstratio', 'og1108al', buildversion)
+                            // sh 'docker login sania.azurecr.io -u $ACRCRED_USR -p $ACRCRED_PSW'
+                            // sh 'docker tag sanitas-dental-bot-clinics sania.azurecr.io/sanitas-dental-bot-clinics:' + buildversion
+                            // sh 'docker push sania.azurecr.io/sanitas-dental-bot-clinics:' + buildversion
+                        }
+
+                        echo 'End push image to repo 2'
+                    }
+                }
             }
+
+            post {
+                success {
+                    echo("correcto")
+                    //slackSend (channel: '#datalake-jenkins', color: '#00FF00', message: 'SUCCESSFUL \n\t Build Number: ' + env.BUILD_NUMBER + '\n\t Job: ' + env.JOB_NAME + '\n\t Build Url: ' + env.BUILD_URL)
+                }
+
+                failure {
+                    echo("error")
+                    //slackSend (channel: '#datalake-jenkins', color: '#FF0000', message: 'FAILURE \n\t Build Number: ' + env.BUILD_NUMBER + '\n\t Job: ' + env.JOB_NAME + '\n\t Build Url: ' + env.BUILD_URL)
+                }
+            }
+        }catch (err){
+            currentBuild.result = 'FAILED'
+            throw err
         }
+
     }
-
-    post {
-        success {
-            echo("correcto")
-            //slackSend (channel: '#datalake-jenkins', color: '#00FF00', message: 'SUCCESSFUL \n\t Build Number: ' + env.BUILD_NUMBER + '\n\t Job: ' + env.JOB_NAME + '\n\t Build Url: ' + env.BUILD_URL)
-        }
-
-        failure {
-            echo("error")
-            //slackSend (channel: '#datalake-jenkins', color: '#FF0000', message: 'FAILURE \n\t Build Number: ' + env.BUILD_NUMBER + '\n\t Job: ' + env.JOB_NAME + '\n\t Build Url: ' + env.BUILD_URL)
-        }
-    }
-
 }
