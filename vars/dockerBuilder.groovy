a5f53b0513661afd9bad8756d206dafe5ee65ef0
Oscar Gomez <ogomez@stratio.com>
2017-09-05 11:51:18 +0200

    test

--- vars/dockerBuilder.groovy
+++ vars/dockerBuilder.groovy
@@ -1,6 +1,6 @@
 def call(String repo,String containerName, String user, String password) {
 
     def login= action('docker login '+"${repo} "+' -u '+"${user}"+' -p ' +"${password}")
-    def tag = action('docker tag'+"${containerName}"+' '+"${repo}"+'/'+"${containerName}"+ getNodeVersion)
+    def tag = action('docker tag '+"${containerName}"+' '+"${repo}"+'/'+"${containerName}"+ getNodeVersion)
     return login + tag
 }
