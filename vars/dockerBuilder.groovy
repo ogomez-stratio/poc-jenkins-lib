d80cbe50440b7f46821f4afd54664cba1c27c961
Oscar Gomez <ogomez@stratio.com>
2017-09-05 13:04:24 +0200

    getNodeVersion test

--- vars/dockerBuilder.groovy
+++ vars/dockerBuilder.groovy
@@ -2,7 +2,7 @@ def call(String repo,String containerName, String user, String password, String 
 
     def build = action('docker build -t '+"${containerName }"+' .')
     def login= action('docker login '+' -u '+"${user}"+' -p ' +"${password}")
-    def tag = action('docker tag '+"${containerName}"+' '+"${repo}"+'/'+"${containerName}"+':'+ getNodeVersion)
-    def push = action('docker push '+"${repo}"+'/'+"${containerName}"+':'+getNodeVersion)
+    def tag = action('docker tag '+"${containerName}"+' '+"${repo}"+'/'+"${containerName}"+':'+ getNodeVersion())
+    def push = action('docker push '+"${repo}"+'/'+"${containerName}"+':'+getNodeVersion())
     return build + login + tag + push
 }
