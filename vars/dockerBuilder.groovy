c407162003bc4531e44ed6d6e375faeac76a5cd6
Oscar Gomez <ogomez@stratio.com>
2017-09-05 12:14:12 +0200

    build version hardcoded

--- vars/dockerBuilder.groovy
+++ vars/dockerBuilder.groovy
@@ -1,6 +1,6 @@
 def call(String repo,String containerName, String user, String password, String buildVersion) {
 
     def login= action('docker login '+"${repo} "+' -u '+"${user}"+' -p ' +"${password}")
-    def tag = action('docker tag '+"${containerName}"+' '+"${repo}"+'/'+"${containerName}"+ buildVersion)
+    def tag = action('docker tag '+"${containerName}"+' '+"${repo}"+'/'+"${containerName}"+':'+ buildVersion)
     return login + tag
 }
