e19664e4a61737b971a6ee0ebc3a73a4faf44b1e
Oscar Gomez <ogomez@stratio.com>
2017-09-05 14:27:17 +0200

    pipeline as code test

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -31,63 +31,50 @@ def call(body) {
 
         try {
 
-            stages {
-                stage('build') {
-                    steps {
-                        script {
-                            echo yarnBuilder()
-                        }
-                    }
-                }
-
-                stage('publish untagged build') {
-                    when {
-                        expression { env.TAG_NAME == null || !(env.TAG_NAME ==~ /^v\d+\.\d+\.\d+$/) }
-                    }
-                    steps {
-                        echo 'Start push image to repo'
-
-                        script {
-                            // def props = readJSON file: 'package.json'
-                            // def buildversion = props.version + '.build-' + env.BUILD_NUMBER
-                            echo dockerBuilder('ogomezstratio', 'test-node-app', 'ogomezstratio', 'og1108al', 'v1.0.0')
-                        }
-
-                        echo 'End push image to repo'
-                    }
-                }
-
-                stage('publish tagged release') {
-                    when {
-                        expression { env.TAG_NAME ==~ /^v\d+\.\d+\.\d+$/ }
-                    }
-                    steps {
-                        echo 'Start push image to repo 2'
-
-                        script {
-                            def buildversion = env.TAG_NAME
-                            echo dockerBuilder('ogomezstratio', 'test-node-app', 'ogomezstratio', 'og1108al', buildversion)
-                            // sh 'docker login sania.azurecr.io -u $ACRCRED_USR -p $ACRCRED_PSW'
-                            // sh 'docker tag sanitas-dental-bot-clinics sania.azurecr.io/sanitas-dental-bot-clinics:' + buildversion
-                            // sh 'docker push sania.azurecr.io/sanitas-dental-bot-clinics:' + buildversion
-                        }
-
-                        echo 'End push image to repo 2'
+            stage('build') {
+                steps {
+                    script {
+                        echo yarnBuilder()
                     }
                 }
             }
 
-            post {
-                success {
-                    echo("correcto")
-                    //slackSend (channel: '#datalake-jenkins', color: '#00FF00', message: 'SUCCESSFUL \n\t Build Number: ' + env.BUILD_NUMBER + '\n\t Job: ' + env.JOB_NAME + '\n\t Build Url: ' + env.BUILD_URL)
+            stage('publish untagged build') {
+                when {
+                    expression { env.TAG_NAME == null || !(env.TAG_NAME ==~ /^v\d+\.\d+\.\d+$/) }
                 }
+                steps {
+                    echo 'Start push image to repo'
 
-                failure {
-                    echo("error")
-                    //slackSend (channel: '#datalake-jenkins', color: '#FF0000', message: 'FAILURE \n\t Build Number: ' + env.BUILD_NUMBER + '\n\t Job: ' + env.JOB_NAME + '\n\t Build Url: ' + env.BUILD_URL)
+                    script {
+                        // def props = readJSON file: 'package.json'
+                        // def buildversion = props.version + '.build-' + env.BUILD_NUMBER
+                        echo dockerBuilder('ogomezstratio', 'test-node-app', 'ogomezstratio', 'og1108al', 'v1.0.0')
+                    }
+
+                    echo 'End push image to repo'
                 }
             }
+
+            stage('publish tagged release') {
+                when {
+                    expression { env.TAG_NAME ==~ /^v\d+\.\d+\.\d+$/ }
+                }
+                steps {
+                    echo 'Start push image to repo 2'
+
+                    script {
+                        def buildversion = env.TAG_NAME
+                        echo dockerBuilder('ogomezstratio', 'test-node-app', 'ogomezstratio', 'og1108al', buildversion)
+                        // sh 'docker login sania.azurecr.io -u $ACRCRED_USR -p $ACRCRED_PSW'
+                        // sh 'docker tag sanitas-dental-bot-clinics sania.azurecr.io/sanitas-dental-bot-clinics:' + buildversion
+                        // sh 'docker push sania.azurecr.io/sanitas-dental-bot-clinics:' + buildversion
+                    }
+
+                    echo 'End push image to repo 2'
+                }
+            }
+
         }catch (err){
             currentBuild.result = 'FAILED'
             throw err
