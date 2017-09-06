58294ba2388a34f42422720f34eabf13ccbea237
Oscar Gomez <ogomez@stratio.com>
2017-09-06 10:23:35 +0200

    pipeline as code test

--- vars/dockerBuilder.groovy
+++ vars/dockerBuilder.groovy
@@ -1,14 +1,17 @@
 def call(String dockerRepo,String containerName, String dockerUser, String dockerPassword, String version) {
 
+    def login
+    def tag
+    def push
     def build = action('docker build -t '+"${containerName }"+' .')
     if (dockerRepo != null || dockerRepo == '') {
-        def login = action('docker login ' + ' -u ' + "${dockerUser}" + ' -p ' + "${dockerPassword}")
-        def tag = action('docker tag ' + "${containerName}" + ' ' + "${dockerUser}" + '/' + "${containerName}" + ':' + version)
-        def push = action('docker push ' + "${dockerUser}" + '/' + "${containerName}" + ':' + version)
+        login = action('docker login ' + ' -u ' + "${dockerUser}" + ' -p ' + "${dockerPassword}")
+        tag = action('docker tag ' + "${containerName}" + ' ' + "${dockerUser}" + '/' + "${containerName}" + ':' + version)
+        push = action('docker push ' + "${dockerUser}" + '/' + "${containerName}" + ':' + version)
     } else{
-        def login = action('docker login ' + ' -u '+"${dockerRepo}" + "${dockerUser}" + ' -p ' + "${dockerPassword}")
-        def tag = action('docker tag ' + "${containerName}" + ' ' + "${dockerRepo}" + '/' + "${containerName}" + ':' + version)
-        def push = action('docker push ' + "${dockerRepo}" + '/' + "${containerName}" + ':' + version)
+        login = action('docker login ' + ' -u '+"${dockerRepo}" + "${dockerUser}" + ' -p ' + "${dockerPassword}")
+        tag = action('docker tag ' + "${containerName}" + ' ' + "${dockerRepo}" + '/' + "${containerName}" + ':' + version)
+        push = action('docker push ' + "${dockerRepo}" + '/' + "${containerName}" + ':' + version)
     }
     return build + login + tag + push
 }
