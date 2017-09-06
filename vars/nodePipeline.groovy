f4887a42e795ade3c37b6bd22d65e3c39a186805
Oscar Gomez <ogomez@stratio.com>
2017-09-06 10:57:14 +0200

    pipeline as code test

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -31,7 +31,7 @@ def call(body) {
 
         currentBuild.result = 'SUCCESS'
 
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${config.acrCredentials}",
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'docker-credentials',
                           usernameVariable: 'ACRCRED_USR', passwordVariable: 'ACRCRED_PWD']])
 
         try {
