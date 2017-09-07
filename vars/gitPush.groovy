1bbba700a10daf0c885e432877cea29616673dd3
Oscar Gomez <ogomez@stratio.com>
2017-09-07 14:40:30 +0200

    pipeline as code test

--- vars/gitPush.groovy
+++ vars/gitPush.groovy
@@ -2,16 +2,9 @@ def call(String gitRepo, String gitCredentialsId){
 
     withCredentials([usernamePassword(credentialsId: gitCredentialsId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
 
-
+        action('git add .')
         action('git commit -m \"Jenkins Build: '+env.BUILD_NUMBER+' \"')
         action('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@'+gitRepo+' master')
-
-
-        if (env.TAG_NAME == null || !(env.TAG_NAME ==~ /^v\d+\.\d+\.\d+$/)) {
-
-            action("git tag -a some_tag -m 'Jenkins'")
-            action('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@' + gitRepo + '--tags')
-        }
 
     }
 }
