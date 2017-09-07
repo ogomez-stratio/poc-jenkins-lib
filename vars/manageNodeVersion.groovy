56f86a2f2637d70fa4a50172e2f47468bf62f1a0
Oscar Gomez <ogomez@stratio.com>
2017-09-07 11:03:22 +0200

    pipeline as code test

--- vars/manageNodeVersion.groovy
+++ vars/manageNodeVersion.groovy
@@ -9,41 +9,39 @@ def call() {
 
     def parser = /(\d+\.)(\d+\.)(\d)/
     def match = (props.version =~ parser)
+    def cleanVersion
 
-    if(match.find()) {
-
-        echo "it matches"
-
-        if (env.TAG_NAME == null || !(env.TAG_NAME ==~ /^v\d+\.\d+\.\d+$/)){
-
-            nextVersion = match.group() + '.build-' + env.BUILD_NUMBER
-
-            echo "next build:"+nextVersion
-
-        } else{
-
-            nextVersion = match.group()
-            echo "next tag: "+nextVersion
-        }
-
-        props.version = nextVersion
-
-        def jsonOut = JsonOutput.toJson(props)
-
-        echo jsonOut
-
-        writeFile(file:'package.json', text: jsonOut)
-
-        echo "despues del write"
-
-        return nextVersion
-
-    } else {
-
+    if(match.find())
+        cleanVersion = match.group()
+    else{
         echo "error"
-
         return "error"
     }
+    echo "it matches"
 
+    if (env.TAG_NAME == null || !(env.TAG_NAME ==~ /^v\d+\.\d+\.\d+$/)){
+
+        nextVersion = cleanVersion + '.build-' + env.BUILD_NUMBER
+
+        echo "next build:"+nextVersion
+
+    } else{
+
+        nextVersion = cleanVersion
+        echo "next tag: "+nextVersion
+    }
+
+    props.version = nextVersion
+
+    def jsonOut = JsonOutput.toJson(props)
+
+    echo jsonOut
+
+    writeFile(file:'package.json', text: jsonOut)
+
+    echo "despues del write"
+
+    return nextVersion
 
 }
+
