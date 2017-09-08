97bd9aaed47d961b178d91889ecb90819a3b02d0
Oscar Gomez <ogomez@stratio.com>
2017-09-08 08:34:09 +0200

    pipeline as code test

--- vars/gitPush.groovy
+++ vars/gitPush.groovy
@@ -3,7 +3,7 @@ def call(String gitRepo, String gitCredentialsId){
     withCredentials([usernamePassword(credentialsId: gitCredentialsId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
 
         //def add = action('git add .')
-        //def commit = action('git commit -m \"Jenkins Build: '+env.BUILD_NUMBER+' \"')
+        def commit = action('git commit -a -m \"Jenkins Build: '+env.BUILD_NUMBER+' \"')
         def status = action('git status')
         def remote = action('git remote -v')
 
