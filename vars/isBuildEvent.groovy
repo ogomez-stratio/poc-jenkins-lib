e3c2b16dacbc8a201f0008934d2734e7ef45ad61
Oscar Gomez <ogomez@stratio.com>
2017-09-18 08:14:46 +0200

    pipeline as code test

--- vars/isBuildEvent.groovy
+++ vars/isBuildEvent.groovy
@@ -2,9 +2,13 @@ def Boolean call(String script) {
 
     def gitCommitMessage = action('git log -1 --pretty=%B')
 
-    if (gitCommitMessage =~ 'Jenkins Build: ')
+    if (gitCommitMessage =~ 'Jenkins Build: ') {
+        echo('commit true')
         return true
-    else
+    }
+    else {
+        echo('commit false')
         return false
+    }
 
 }
