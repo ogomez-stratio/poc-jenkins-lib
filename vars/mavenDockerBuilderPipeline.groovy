55850c969a5a8ba8cd4ebb9b863d85439d78d500
Oscar Gomez <ogomez@stratio.com>
2017-10-04 08:15:11 +0200

    correct refactor errors

--- vars/mavenDockerBuilderPipeline.groovy
+++ vars/mavenDockerBuilderPipeline.groovy
@@ -47,50 +47,50 @@ def call(body) {
 
                 echo mavenBuilder("${config.pomPath}")
             }
-
-            /**
-             * Build, Tag and publish the Docker Image into Docker Repository (Currently Azure Container Registry)
-             */
-            stage('publish build to develop') {
-
-                withCredentials([usernamePassword(credentialsId: "${config.registryCredentialsId}",
-                        usernameVariable: 'ACR_USR', passwordVariable: 'ACR_PWD')]) {
-                    echo 'Start to push image to repo'
-                    script {
-                        echo dockerMavenBuilder("${config.pomPath}")
-                        echo dockerMavenPublisher("${config.dockerBuildRepo}", "${config.containerName}", "${env.ACR_USR}",
-                                "${env.ACR_PWD}", env.NEXT_VERSION)
-                    }
-                    echo 'End to push image to repo'
-
-                }
-            }
-
-            /**
-             * Build, Tag and publish the Docker Image into Docker Repository (Currently Azure Container Registry)
-             */
-            stage('publish RC Build') {
-
-                env.NEXT_VERSION = manageMavenVersion("RC", "${config.pomPath}")
-
-                withCredentials([usernamePassword(credentialsId: "${config.registryCredentialsId}",
-                        usernameVariable: 'ACR_USR', passwordVariable: 'ACR_PWD')]) {
-                    echo 'Start to push image to repo'
-                    script {
-                        milestone 1
-                        timeout(time: 10, unit: 'MINUTES') {
-                            input message: "You're going to publish RC:"+env.NEXT_VERSION + " to: "+ "${config.dockerRcRepo}"+" Do you want to continue?"
-                        }
-                        // this will kill any job which is still in the input step
-                        echo dockerMavenPublisher("${config.dockerRcRepo}", "${config.containerName}", "${env.ACR_USR}",
-                                "${env.ACR_PWD}", env.NEXT_VERSION,"${config.pomPath}")
-
-                        milestone 2
-                    }
-                    echo 'End to push image to repo'
-
-                }
-            }
+//
+//            /**
+//             * Build, Tag and publish the Docker Image into Docker Repository (Currently Azure Container Registry)
+//             */
+//            stage('publish build to develop') {
+//
+//                withCredentials([usernamePassword(credentialsId: "${config.registryCredentialsId}",
+//                        usernameVariable: 'ACR_USR', passwordVariable: 'ACR_PWD')]) {
+//                    echo 'Start to push image to repo'
+//                    script {
+//                        echo dockerMavenBuilder("${config.pomPath}")
+//                        echo dockerMavenPublisher("${config.dockerBuildRepo}", "${config.containerName}", "${env.ACR_USR}",
+//                                "${env.ACR_PWD}", env.NEXT_VERSION)
+//                    }
+//                    echo 'End to push image to repo'
+//
+//                }
+//            }
+//
+//            /**
+//             * Build, Tag and publish the Docker Image into Docker Repository (Currently Azure Container Registry)
+//             */
+//            stage('publish RC Build') {
+//
+//                env.NEXT_VERSION = manageMavenVersion("RC", "${config.pomPath}")
+//
+//                withCredentials([usernamePassword(credentialsId: "${config.registryCredentialsId}",
+//                        usernameVariable: 'ACR_USR', passwordVariable: 'ACR_PWD')]) {
+//                    echo 'Start to push image to repo'
+//                    script {
+//                        milestone 1
+//                        timeout(time: 10, unit: 'MINUTES') {
+//                            input message: "You're going to publish RC:"+env.NEXT_VERSION + " to: "+ "${config.dockerRcRepo}"+" Do you want to continue?"
+//                        }
+//                        // this will kill any job which is still in the input step
+//                        echo dockerMavenPublisher("${config.dockerRcRepo}", "${config.containerName}", "${env.ACR_USR}",
+//                                "${env.ACR_PWD}", env.NEXT_VERSION,"${config.pomPath}")
+//
+//                        milestone 2
+//                    }
+//                    echo 'End to push image to repo'
+//
+//                }
+//            }
 
         } catch (err) {
             currentBuild.result = 'FAILED'
